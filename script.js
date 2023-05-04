5d780fd101f47f423a648fa6a61ccf7fbda8d13d
shauek97 <shauek97@gmail.com>
2023-05-04 20:04:20 +0200

    Defined first consts

---
+++ script.js
@@ -0,0 +1,23 @@
+const CeBtn = document.querySelector("#ce");
+const backBtn = document.querySelector("#back");
+const oneBtn = document.querySelector("#one");
+const twoBtn = document.querySelector("#two");
+const threeBtn = document.querySelector("#three");
+const fourBtn = document.querySelector("#four");
+const fiveBtn = document.querySelector("#five");
+const sixBtn = document.querySelector("#six");
+const sevenBtn = document.querySelector("#seven");
+const eightBtn = document.querySelector("#eight");
+const nineBtn = document.querySelector("#nine");
+const zeroBtn = document.querySelector("#zero");
+const commaBtn = document.querySelector("#comma");
+const plusBtn = document.querySelector("#plus");
+const minusBtn = document.querySelector("#minus");
+const multiplyBtn = document.querySelector("#multiply");
+const splitBtn = document.querySelector("#split");
+const equalBtn = document.querySelector("#equal");
+const mathResult = document.querySelector("#math-result");
+
+function calcEngine(){
+    
+}
