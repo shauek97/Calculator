3effb3d0a6cc23ac276c176d4669c75973d3662a
shauek97 <shauek97@gmail.com>
2023-05-08 16:07:07 +0200

    Made separated arrays for display and for numbers saving

--- script.js
+++ script.js
@@ -20,10 +20,14 @@ const equalBtn = document.querySelector("#equal");
 const mathResult = document.querySelector("#math-result");
 const ingredients = document.querySelectorAll('.ingredient');
 const operators = document.querySelectorAll('.operator')
+const digits = document.querySelectorAll('.digit')
 
 function calcEngine(){
-    const clickedDigits = [];
-    const typedNumbers = [];
+    let displayIngredients = [];    
+    let clickedIngredients = [];
+    let typedNumbers = [];
+    
+
 //place beside clicked numbers in mathresult place
 //how?
 //
@@ -32,25 +36,31 @@ function calcEngine(){
     
     for(let ingredient of ingredients){
         
-        let cellInnerHtml = ingredient.getElementsByTagName('h2')[0].innerHTML;
-        console.log(cellInnerHtml)
+        let ingredientInnerHtml = ingredient.getElementsByTagName('h2')[0].innerHTML;
+        console.log(ingredientInnerHtml)
 
        
         ingredient.addEventListener('click', function(){     
            
-            clickedDigits.push(cellInnerHtml)     
-               
-            mathResult.innerHTML = clickedDigits.join('');
+            clickedIngredients.push(ingredientInnerHtml) 
+            displayIngredients.push(ingredientInnerHtml)
+            let displayedCombination = displayIngredients.join('');
 
-            console.log(clickedDigits)
-            console.log(clickedDigits.join(''))
+            mathResult.innerHTML = displayedCombination;
+
+            console.log(clickedIngredients)
+            console.log(clickedIngredients.join(''))
         }); 
     }
 
     for(let operator of operators){
         operator.addEventListener('click', function(){
-            typedNumbers.push(clickedDigits.join(''));
-            console.log(parseFloat(typedNumbers));
+            
+            let typedNumber = parseFloat(clickedIngredients.join(''));
+            typedNumbers.push(typedNumber);
+            clickedIngredients = [];
+            console.log(typedNumber);
+            console.log(typedNumbers)
         });
     };
     
@@ -64,3 +74,4 @@ calcEngine();
 // klikniecie operatora ma wtłoczyć liczbe do tablicy - wtłacza
 // jednoczesnie ma wyjebac samego operatora z tej tablicy
 // kliknięcie = ma wykonywać działanie pomiędzy jedną liczbą w tablicy a drugą
+
