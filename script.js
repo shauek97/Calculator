cb563a1a7dc094c64b813c4ce9ce40531b39acd1
shauek97 <shauek97@gmail.com>
2023-05-09 11:43:46 +0200

    Completed project v 1.0

--- script.js
+++ script.js
@@ -1,61 +1,51 @@
 function calcEngine(){
-    const CeBtn = document.querySelector("#ce");
+
+    const ceBtn = document.querySelector("#ce");
+    const backBtn = document.querySelector("#back");
     const equalBtn = document.querySelector("#equal");
 
     const mathResult = document.querySelector("#math-result");
     const ingredients = document.querySelectorAll('.ingredient');
-    const operators = document.querySelectorAll('.operator');
-    const digits = document.querySelectorAll('.digit');
 
-    
-    let displayIngredients = [];    
     let clickedIngredients = [];
-    let typedNumbers = [];
-    let mergedCalculation = clickedIngredients.join('');
 
-//place beside clicked numbers in mathresult place
-//how?
-//
-//create variable for inputed number
-//how to create it?
-    
     for(let ingredient of ingredients){
         
         let ingredientInnerHtml = ingredient.getElementsByTagName('h2')[0].innerHTML;
    
         ingredient.addEventListener('click', function(){     
            
-            clickedIngredients.push(ingredientInnerHtml) 
-            displayIngredients.push(ingredientInnerHtml)
-            let displayedCombination = displayIngredients.join('');
+            clickedIngredients.push(ingredientInnerHtml);
+            
+            let mergedCalculation = clickedIngredients.join('');
+            mathResult.innerHTML = mergedCalculation;
 
-            mathResult.innerHTML = displayedCombination;
-
-            console.log(clickedIngredients)
-            console.log(clickedIngredients.join(''))
+            console.log(clickedIngredients);
+            console.log(clickedIngredients.join(''));
         }); 
-    }
-
-    for(let operator of operators){
-        operator.addEventListener('click', function(){
-            
-            let typedNumber = parseFloat(clickedIngredients.join(''));
-            typedNumbers.push(typedNumber);
-            typedNumbers.push(this.getElementsByTagName('h2')[0].innerHTML)
-            
-            console.log(mergedCalculation)
-            console.log(typedNumber);
-            console.log(typedNumbers)
-        });
     };
 
-    equalBtn.addEventListener('click', function(){
+    ceBtn.addEventListener('click', function(){
+        clickedIngredients = [];
+        let mergedCalculation = clickedIngredients.join('');
+        mathResult.innerHTML = mergedCalculation;
+    })
 
+    backBtn.addEventListener('click', function(){
+        console.log('dsad')
+        clickedIngredients.pop(-1);
+        let mergedCalculation = clickedIngredients.join('');
+        mathResult.innerHTML = mergedCalculation;
+        console.log(clickedIngredients);
+    });
+
+    equalBtn.addEventListener('click', function(){
        let finalResult = eval(clickedIngredients.join(''));
        mathResult.innerHTML = finalResult;
-
-    })
-    
+       clickedIngredients = [];
+       clickedIngredients.push(finalResult.toString().(''))
+       console.log(clickedIngredients)
+    });
 };
 
 calcEngine();
